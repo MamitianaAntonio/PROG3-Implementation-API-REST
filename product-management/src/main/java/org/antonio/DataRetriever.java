71ee238eea3068738d598a52424b18e65dd74ab0
Mamitiana Antonio <ratolojanaharyantonio1@gmail.com>
2025-12-08 19:34:36 +0300

    feat : implement getProductList on DataRetriever class using pagination

--- product-management/src/main/java/org/antonio/DataRetriever.java
+++ product-management/src/main/java/org/antonio/DataRetriever.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataRetriever {
+  // code to get all categories
   public List<Category> getAllCategories() throws SQLException {
     List<Category> categories = new ArrayList<>();
     String sql = "SELECT id, name FROM Product_category";
@@ -25,5 +26,50 @@ public class DataRetriever {
       }
     }
     return categories;
+  }
+
+  // code to get product list
+  List<Product> getProductList(int page, int size) throws SQLException {
+    List<Product> products = new ArrayList<>();
+    int offset = (page - 1) * size;
+    String sql = """
+        SELECT  p.id AS product_id,
+            p.name AS product_name,
+            p.creation_datetime,
+            c.id AS category_id,
+            c.name AS category_name
+        FROM Product p
+        LEFT JOIN Product_category c ON c.product_id = p.id
+        ORDER BY p.id
+        LIMIT ? OFFSET ?;
+        """;
+
+    try (Connection connection = DBConnection.getConnection();
+      PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, size);
+      statement.setInt(2, offset);
+
+      ResultSet resultSet = statement.executeQuery(); {
+        while(resultSet.next()) {
+          Category category = null;
+          int categoryId = resultSet.getInt("category_id");
+          String categoryName = resultSet.getString("category_name");
+
+          if (categoryName != null) {
+            category = new Category(categoryId, categoryName);
+          }
+
+          Product product = new Product (
+              resultSet.getInt("product_id"),
+              resultSet.getString("product_name"),
+              resultSet.getTimestamp("creation_datetime").toInstant(),
+              category
+          );
+
+          products.add(product);
+        }
+      }
+    }
+    return products;
   }
 }
